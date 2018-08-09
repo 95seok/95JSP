ae9c88202f23fb59a697e078d4f88c2c08dba12a
95seok <seok270@naver.com>
2018-08-09 11:48:19 +0900

    출력 문구

--- Hello_WORLD/src/HelloWorld.java
+++ Hello_WORLD/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Time is 1:06 AM");
+		System.out.println("¾¾¹ã¹Ù");
 	}
 }
